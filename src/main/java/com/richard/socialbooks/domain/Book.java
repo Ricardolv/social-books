e218c6d1865053186adb9a08c331506d54bb6c58
Ricardolv <richardluizv@gmail.com>
2017-06-18 02:23:55 -0300

    Utilizando a anotação @JsonInclude

--- src/main/java/com/richard/socialbooks/domain/Book.java
+++ src/main/java/com/richard/socialbooks/domain/Book.java
@@ -1,6 +1,7 @@
 package com.richard.socialbooks.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -12,13 +13,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class Book {
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Long id;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @NonNull
     private String name;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private LocalDate publication;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String publishers;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<Comments> comments;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String author;
 
 }
