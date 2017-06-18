d8bccf39ce5701d1d8370bd6b73ffad5f39d8ae3
Ricardolv <richardluizv@gmail.com>
2017-06-18 03:23:16 -0300

    Salvando o recurso Livro a partir de um POST

--- src/main/java/com/richard/socialbooks/resources/BooksResources.java
+++ src/main/java/com/richard/socialbooks/resources/BooksResources.java
@@ -3,9 +3,7 @@ package com.richard.socialbooks.resources;
 import com.richard.socialbooks.domain.Book;
 import com.richard.socialbooks.repository.BooksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class BooksResources {
     @GetMapping
     public List<Book> list() {
         return booksRepository.findAll();
+    }
+
+    @PostMapping
+    public void save(@RequestBody Book book) {
+        booksRepository.save(book);
     }
 }
